c59cefdc1ff422bb0b7ef09cad8a5fbd9be013da
Andre Bellefine Lindeløff <andrel@mesan.no>
2017-07-01 13:58:59 +0200

    test

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,31 @@
 const path = require('path');
+
 module.exports = {
     entry: path.resolve(__dirname, './src/index.js'),
     output: {
         path: path.resolve(__dirname, './dist'),
         filename: 'app.bundle.js',
+    },
+    watch: true,
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: [/node_modules/],
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['react', 'env']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
     }
 };
